f6eb93786b530d5faf8fdf31cb2bddba7b24e7a7
Jeff Seo <jeffseo.js@gmail.com>
2020-09-28 22:29:28 -0700

    Lesson 20: Setup project with assets and a simple Collection View to display cards

---
+++ MatchApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MatchApp
+//
+//  Created by Jeff Seo on 2020-09-28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
