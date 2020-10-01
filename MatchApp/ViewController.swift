1a5b2432378e1085d0faea0ceb43c8b079f29d39
Jeff Seo <jeffseo.js@gmail.com>
2020-09-30 20:20:49 -0700

    Lesson 23: Implement the delegate and protocol pattern by assigning the viewController as the collectionView data source and delegate. Implement the delegate protocol functions.

--- MatchApp/ViewController.swift
+++ MatchApp/ViewController.swift
@@ -7,7 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+  
+  @IBOutlet weak var collectionView: UICollectionView!
   
   let model = CardModel()
   var cardsArray = [Card]()
@@ -17,8 +19,33 @@ class ViewController: UIViewController {
     // Do any additional setup after loading the view.
     
     cardsArray = model.getCards()
+    
+    // Set the view controller as the datasource and delegate of the collection view
+    collectionView.dataSource = self
+    collectionView.delegate = self
   }
 
+  // MARK: - Collection View Delegate Methods
+  
+  // CollectionView uses this function to get the number of items
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    
+    // Return number of cards
+    return cardsArray.count
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    // IndexPath represents which cell in the collection view we are targetting
+
+    // Get a Cell
+    // The withReuseIdentfier is the name given to the prototype cell
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+    
+    // TODO: Finish configuring the cell
+    
+    // Return it
+    return cell
+  }
 
 }
 
