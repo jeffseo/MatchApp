e476928df1f0ec2c5f51f11a43711a1899a6aa67
Jeff Seo <jeffseo.js@gmail.com>
2020-10-02 21:17:43 -0700

    Lesson 26: Add logic to transition (flip) card from back to front and vice versa with use of transitions and the delegate function to handle taps

--- MatchApp/CardCollectionViewCell.swift
+++ MatchApp/CardCollectionViewCell.swift
@@ -8,7 +8,45 @@
 import UIKit
 
 class CardCollectionViewCell: UICollectionViewCell {
+  
   @IBOutlet weak var frontImageView: UIImageView!
+  
   @IBOutlet weak var backImageView: UIImageView!
   
+  var card:Card?
+  
+  func configureCell(card:Card) {
+    
+    // Keep track of the card this cell represents
+    self.card = card
+    
+    // Set the front image view to the image that represents the card
+    frontImageView.image = UIImage(named: card.imageName)
+    
+    // Reset the state of the cell by checking the flipped status of the card and then showing the front or the back imageview accordingly
+    if card.isFlipped == true {
+      // show the front image view
+      flipUp(speed: 0)
+    }
+    else
+    {
+      flipDown(speed: 0)
+    }
+  }
+  
+  func flipUp(speed: TimeInterval = 0.3) {
+    // Flip up animation
+    UIView.transition(from: backImageView, to: frontImageView, duration: speed, options: [.showHideTransitionViews, .transitionFlipFromLeft], completion: nil)
+    
+    // Set the status of the card
+    card?.isFlipped = true
+  }
+  
+  func flipDown(speed: TimeInterval = 0.3) {
+    // Flip down animation
+    UIView.transition(from: frontImageView, to: backImageView, duration: speed, options: [.showHideTransitionViews, .transitionFlipFromLeft], completion: nil)
+    
+    // Set the status of the card
+    card?.isFlipped = false
+  }
 }
--- MatchApp/ViewController.swift
+++ MatchApp/ViewController.swift
@@ -32,19 +32,39 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     
     // Return number of cards
     return cardsArray.count
-  }
+  }	
   
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
     // IndexPath represents which cell in the collection view we are targetting
 
     // Get a Cell
     // The withReuseIdentfier is the name given to the prototype cell
-    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+    // dequeueResuableCell returns a UICell object. We must cast it to the object we want which inherits the UICell which is CardCollectionViewCell (set manually in prototype property)
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath) as! CardCollectionViewCell
     
-    // TODO: Finish configuring the cell
+    // Get the card from the card array
+    let card = cardsArray[indexPath.row]
+    
+    cell.configureCell(card: card)
     
     // Return it
     return cell
+  }
+  
+  // This function is the delegate function that is responsible for handling taps
+  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    // Get a reference to the cell that was tapped
+    let cell = collectionView.cellForItem(at: indexPath) as? CardCollectionViewCell
+    
+    // Check the status of the card to determine how to flip it
+    if cell?.card?.isFlipped == false {
+      // Flip the card up
+      cell?.flipUp()
+    }
+    else {
+      // Flip the card down
+      cell?.flipDown()
+    }
   }
 
 }
